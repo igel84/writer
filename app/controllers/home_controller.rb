43ab392f9e34a59112a6e6d583bf8f8cde1171f4
igel84 <igel.yulian@mail.ru>
2017-12-11 21:38:54 +0300

    Fix user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,19 @@
 class HomeController < ApplicationController
+  require 'net/http'
+  require 'uri'
+  require 'open-uri'
+  require 'httparty'
+
   def index
+    if current_user.token.present?
+      url = "https://api.vk.com/method/groups.get?filter=editor&access_token=#{current_user.token}&v=V"
+      uri = URI::encode(url)
+      response = HTTParty.get(uri)
+      @result = response
+      # response["response"].each do |group_id|
+      #   @result += group_id
+      # end
+    end
     # render inline: "<h6>Ok!</h6>"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, :skip => [:registrations]
   as :user do
     get 'users/new' => 'users/registrations#new', :as => 'new_user_registration'
+    post 'users' => 'users/registrations#create'
     get 'users/edit' => 'users/registrations#edit', :as => 'edit_user_registration'
     put 'users' => 'users/registrations#update', :as => 'user_registration'
     get 'users/cancel' => 'users/registrations#cancel', :as => 'cancel_user_registration'
